6721f27d1267b9b2b4106c2caa6b716bc61c7ed7
Celine Tran <celine@Celines-MBP.lan>
2020-09-29 23:27:47 -0700

    commit to 'master'

--- A1/VotingService.java
+++ A1/VotingService.java
@@ -3,23 +3,21 @@ public class VotingService {
 
     }
 
-    int A; 
-    int B;
-    int C; 
-    int D; 
-    int right; 
-    int wrong; 
+    int questionCount; 
 
     public void submission(Student student){
-        
-    }
 
-    public void submissionResults(){
-        System.out.println("A: " + A);
-        System.out.println("B: " + B);
-        System.out.println("C: " + C);
-        System.out.println("D: " + D);
-        System.out.println("Right: " + right);
-        System.out.println("Wrong: " + wrong);
+    }
+    
+    public void runQuestions(String[] question){
+        System.out.println("---------------------------------------");
+        System.out.println("Quesion: " + questionCount++);
+        System.out.println("Choose answer(s): ");
+        System.out.println("A: " + question[1]); 
+        System.out.println("B: " + question[2]); 
+        System.out.println("C: " + question[3]); 
+        System.out.println("D: " + question[4]); 
+        System.out.println("---------------------------------------");
+        
     }
 }
